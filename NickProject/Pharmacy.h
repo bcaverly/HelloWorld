86aa542ca6e325d3d0db9306216847ba51b06154
bcaverly <30443436+bcaverly@users.noreply.github.com>
2017-07-25 14:25:07 -0400

    Add files via upload

---
+++ NickProject/Pharmacy.h
@@ -0,0 +1,13 @@
+#ifndef Pharmacy_H
+#define Pharmacy_H
+
+class Pharmacy{
+private:
+	int medicationCost;
+public:
+	Pharmacy(int);
+	void getCost(int, int);
+	void addCost(int, int);
+
+};
+#endif
